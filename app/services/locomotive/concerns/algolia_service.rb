0974c98025e8f5f0ee150a3298660e5d32191ce3
Joshua Tyree <joshuat@createthebridge.com>
2020-01-24 02:59:03 -0600

    Fixed relationship serialization for algolia

--- app/services/locomotive/concerns/algolia_service.rb
+++ app/services/locomotive/concerns/algolia_service.rb
@@ -3,13 +3,15 @@ require 'algoliasearch'
 module Locomotive
   module Concerns
     module AlgoliaService
+      STATIC_FIELDS = %w(seo_title meta_description meta_keywords created_at updated_at _position _visible _translated)
+
 
       def algolia_client
         @algolia_client ||= Algolia.init(application_id: Locomotive.config[:algolia][:app_id], api_key: Locomotive.config[:algolia][:api_key])
         @algolia_client
       end
 
-      def algolia_fields(content_entry)
+      def serializable_fields(content_entry)
         content_entry.custom_fields_recipe["rules"].map{|rule|
           {
             name: rule["name"],
@@ -19,54 +21,60 @@ module Locomotive
         }
       end
 
+      def serialize_object(content_entry, process_relationships = true)
+        ## Get base fields present on every ContentEntry
+        result = { objectID: content_entry._slug }
+        result = result.merge(content_entry.to_steam.to_hash.slice(STATIC_FIELDS))
+
+        ## Build the serializable fields list
+        fields = serializable_fields(content_entry)
+        relationships = fields.select{|x| %w(many_to_many belongs_to has_many).include?(x[:data_type]) }.map{|y| y[:name]}
+
+        ## Loop the serializable fields and add the data to the results
+        content_hash = content_entry.as_json
+
+        fields.each do |field|
+          if %w(many_to_many belongs_to has_many).include?(field[:data_type]) and process_relationships # Relationship
+
+            result[field[:name]] = add_relationships_to_serialized_object(field, content_entry)
+
+          elsif field[:data_type] == "select"
+
+            option = field[:options].select{|x| x["_id"].to_s == content_hash["#{field[:name]}_id"]}
+            if option.any?
+              result["#{field[:name]}_id"] = content_hash["#{field[:name]}_id"]
+              result[field[:name]] = option[0]["name"]
+            else
+              result["#{field[:name]}_id"] = content_hash["#{field[:name]}_id"]
+            end
+
+          else
+
+            result[field[:name]] = content_hash[field[:name]]
+
+          end
+        end
+
+        return result
+      end
+
+      def add_relationships_to_serialized_object(field, content_entry)
+        items = content_entry.send(field[:name].to_sym)
+        if items.is_a? Array
+          return items.map{|item| serialize_object(item, false) }
+        else
+          return serialize_object(items, false)
+        end
+      end
+
       def add_to_algolia(content_entry)
         return unless content_entry.content_type.algolia_indexing_enabled
 
         algolia_client
         index = Algolia::Index.new("#{content_entry.content_type.slug}--#{content_entry.site.handle}")
-
-        fields = algolia_fields(content_entry)
-
-        obj = {
-          objectID: content_entry._slug,
-          sort_position: content_entry._position
-        }
-
-        obj = obj.merge(content_entry.to_steam.to_hash.slice(
-          "seo_title",
-          "meta_description",
-          "meta_keywords",
-          "created_at",
-          "updated_at",
-          "_visible",
-          "_translated"
-        ))
-
-        relations = fields.select{|x| %w(many_to_many belongs_to has_many).include?(x[:data_type]) }.map{|y| y[:name]}
-        tmp = content_entry.as_json({
-          include: relations
-        })
-
-        fields.each do |field|
-          if field[:data_type] == "select"
-            option = field[:options].select{|x| x["_id"].to_s == tmp["#{field[:name]}_id"]}
-            puts option
-            if option.any?
-              obj["#{field[:name]}_id"] = tmp["#{field[:name]}_id"]
-              obj[field[:name]] = option[0]["name"]
-            else
-              obj["#{field[:name]}_id"] = tmp["#{field[:name]}_id"]
-            end
-          else
-            obj[field[:name]] = tmp[field[:name]]
-          end
-        end
-
-        puts ""
-        puts obj
-        puts ""
-
-        index.save_object(obj)
+        result = serialize_object(content_entry)
+        puts JSON.pretty_generate(result)
+        index.save_object(result)
       end
 
       def remove_from_algolia(content_entry)
